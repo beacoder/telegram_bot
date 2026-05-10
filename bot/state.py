7c31669991a2f1e0989824119462c407f9d42847
陈虎明 <chenhuming@gmail.com>
2026-05-11 00:17:45 +0800

    Implement voice toggle and check functions
    Added voice control functionality with toggle and check methods.

--- bot/state.py
+++ bot/state.py
@@ -3,6 +3,7 @@ from .config import CURRENT_MODEL_KEY
 
 current_model_key = CURRENT_MODEL_KEY
 agent_lock = asyncio.Lock()
+voice_enabled = False
 
 
 def get_model_key():
@@ -12,3 +13,13 @@ def get_model_key():
 def set_model_key(key: str):
     global current_model_key
     current_model_key = key
+
+
+def toggle_voice():
+    global voice_enabled
+    voice_enabled = not voice_enabled
+    return voice_enabled
+
+
+def is_voice_enabled():
+    return voice_enabled
